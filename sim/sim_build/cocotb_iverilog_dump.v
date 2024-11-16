f54393acded62fe0475edafffceb01e08c70b44c
ceronj26 <ceronj26@mit.edu>
2024-11-16 13:31:09 -0500

    Add files via upload

--- sim/sim_build/cocotb_iverilog_dump.v
+++ sim/sim_build/cocotb_iverilog_dump.v
@@ -1,6 +1,6 @@
 module cocotb_iverilog_dump();
 initial begin
-    $dumpfile("C:/Users/nicho/OneDrive/Documents/MIT/Classes/6.205/lab07/sim/sim_build/convolution.fst");
-    $dumpvars(0, convolution);
+    $dumpfile("C:/Users/nicho/OneDrive/Documents/MIT/Classes/6.205/Final Project/ObNoDog/sim/sim_build/circularity.fst");
+    $dumpvars(0, circularity);
 end
 endmodule
