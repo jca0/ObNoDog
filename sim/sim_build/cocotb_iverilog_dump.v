886f532a35e854ab64090c3744b536b5f96349d1
ceronj26 <ceronj26@mit.edu>
2024-11-16 13:32:47 -0500

    Delete sim directory

--- sim/sim_build/cocotb_iverilog_dump.v
+++
@@ -1,6 +0,0 @@
-module cocotb_iverilog_dump();
-initial begin
-    $dumpfile("C:/Users/nicho/OneDrive/Documents/MIT/Classes/6.205/Final Project/ObNoDog/sim/sim_build/circularity.fst");
-    $dumpvars(0, circularity);
-end
-endmodule
